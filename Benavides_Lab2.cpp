a3c63d282d781186caa8db67d3fc75120b482223
Mario Benavides Jr <36014007+MarioBen22@users.noreply.github.com>
2018-10-08 00:31:40 -0500

    Update Benavides_Lab2.cpp
    tidying up spacing

--- Benavides_Lab2.cpp
+++ Benavides_Lab2.cpp
@@ -7,15 +7,12 @@ using namespace std;
 
 int main()
 {
-	double ordered, discount, grand, total; 			// declares variables
+	double ordered, discount, grand, total; 		// declares variables
+	int software = 199.00;					// cost for math later
 	
 	
 	cout << "How many units are being purchased? "; 	// input question
-	cin >> ordered; 									// input
-
-
-	int software = 199.00;								// cost for math later
-	
+	cin >> ordered; 					// input
 	
 	
 	if (ordered >= 1 && ordered <= 9) 			// 0-9 no discount
@@ -25,37 +22,36 @@ int main()
 				grand = total - discount;
 	}	
 
-	else if (ordered >= 10 && ordered <= 19) 	// 10-19
+	else if (ordered >= 10 && ordered <= 19) 		// 10-19
 	{
 		total = ordered * software;
 			discount = total * .20;
 				grand = total - discount;
 	}	
 
-	else if (ordered >=20 && ordered <=49) 		// 20-49
+	else if (ordered >= 20 && ordered <= 49) 		// 20-49
 	{
 		total = ordered * software;
 			discount = total * .30;
 				grand = total - discount;	
 	}
 				
-	else if (ordered >=50 && ordered <=99)		 // 50-99
+	else if (ordered >= 50 && ordered <= 99)		 // 50-99
 	{
 		total = ordered * software;
 			discount = total * .40;
 				grand = total - discount;
 	}
 				
-	else if (ordered >=100) 					// 100+
+	else if (ordered >= 100) 				// 100+
 	{
 		total = ordered * software;
 			discount = total * .50;
 				grand = total - discount;
 	}	
-		
-		
-
-	cout << "You saved $ " << discount << endl; // added a "you saved" section for fun. 
+	
+	
+	cout << "You saved $ " << discount << endl;		 // added a "you saved" section for fun. 
 	cout << "The total cost of the purchase is $ " << grand << endl;
 	
 	return 0;	
